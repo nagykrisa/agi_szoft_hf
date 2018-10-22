6164d6d8c491747b5c52a49edca34b8befdcc8d4
Cser Zoltán <cser.zoltan96@gmail.com>
2018-10-22 18:55:54 +0200

    Node js express added.

---
+++ index.js
@@ -0,0 +1,8 @@
+﻿var express = require('express');
+var app = express();
+
+app.use(express.static('static'));
+
+var server = app.listen(3000, function () {
+    console.log('App running on: localhost:3000/');
+});
